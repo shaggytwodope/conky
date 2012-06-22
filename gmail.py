d583f86f5b04ffee07e330234f68adbbc64a0ea7
n1x4 <lulzer30@gmail.com>
2012-06-22 19:31:16 -0400

    gmail checker

--- gmail.py
+++ gmail.py
@@ -2,8 +2,8 @@ import os
  
 #Enter your username and password below within double quotes
 # eg. username="username" and password="password"
-username="lulzer30"
-password="godspeed123"
+username=""
+password=""
 com="wget -q -O - https://"+username+":"+password+"@mail.google.com/mail/feed/atom --no-check-certificate"
 temp=os.popen(com)
 msg=temp.read()
